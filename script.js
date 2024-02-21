ab391ab6ed4a2ef84f38f92abd19a6fab856be8a
Saptarshi Halder <62690091+saptarshihalder@users.noreply.github.com>
2024-02-21 22:38:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const texts = ['Every Rain Begins With A Single Drop', 'Rain does not fall on one roof alone.', 'Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky.', 'Life Is Not About Storms To Pass, Its About Dancing In The Rain'];
+const randomText = texts[Math.floor(Math.random() * texts.length)];
+
+document.getElementsByClassName('text')[0].innerHTML = randomText;
+
+const redirectionTime = Math.floor(Math.random() * 5) + 4;
+setTimeout(() => {
+    window.location.href = 'https://saptarshihalder.github.io/home/';
+}, redirectionTime * 1000);
