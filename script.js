65c4e4b636cc2e43125d955b0f7c824ec090dabd
Saptarshi Halder <62690091+saptarshihalder@users.noreply.github.com>
2024-02-22 03:13:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const texts = ['Every Rain Begins With A Single Drop', 'Rain does not fall on one roof alone.', 'Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky.','If I were rain, that joins heaven and earth that otherwise never touch, Could I be the rain joining two hearts as well?', 'Life Is Not About Storms To Pass, Its About Dancing In The Rain'];
+const texts = ['Every Rain Begins With A Single Drop', 'Rain does not fall on one roof alone.', 'Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky.', 'Life Is Not About Storms To Pass, Its About Dancing In The Rain', 'If I were rain, that joins heaven and earth that otherwise never touch, Could I be the rain joining two hearts as well?'];
 const randomText = texts[Math.floor(Math.random() * texts.length)];
 
 document.getElementsByClassName('text')[0].innerHTML = randomText;
